935a1a7955de26fe15c7e582c55cb8b5aeea214a
Eric <happyeric76@hotmail.com>
2021-03-17 20:11:01 +0900

    Add nodes part

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,16 @@ import json
 from flask import Flask, jsonify
 import transaction as tx
 import pickle
+import requests
+import threading
 
+main_nodes = set(
+    [
+        "0.0.0.0:5000",
+        "0.0.0.0:5001",
+        "0.0.0.0:5002",
+    ]
+)
 
 class Block:
     def __init__(self, id: int, transaction: bytes, prevHash: int):
@@ -54,10 +63,14 @@ class Block:
         self.hash = hashDec
 
 class Blockchain:
-    def __init__(self):        
+    # def __init__(self):
+    #TODO
+    def __init__(self, host, port):        
         self.blocks = [Block(0, pickle.dumps(tx.Transaction("Genesis", {"Genesis":"Genesis"})) , 0)]
         self.txs = []
         self.UTXOs = []
+        #TODO
+        self.nodes = main_nodes
 
     def addBlock (self, block: Block):
         prevHash = self.blocks[-1].hash
@@ -99,6 +112,15 @@ class Blockchain:
             if block.prevHash == self.blocks[block.index-1].hash:
                 print(f"Block#{str(block.index)} is valid")
 
+    def validateChain(self, chain) -> bool:
+        for block in chain.blocks:
+            if block.prevHash == self.blocks[block.index-1].hash:
+                print(f"Block#{str(block.index)} is valid")
+            else:
+                return False
+        return True
+
+
     def find_Txs(self) -> [str]:
         self.txs = []
         for block in self.blocks:
@@ -106,6 +128,105 @@ class Blockchain:
             tx = pickle.loads(txByte)
             self.txs.append(tx.id)
         return self.txs
+
+    def outputNodes(self):
+        return self.nodes
+
+    def addNodes(self, nodes):
+        self.nodes.update(nodes)
+
+
+    #TODO
+    def syncNode(self, host: str, port: int):
+        main_nodes.add(f"{host}:{port}")
+        syncedNode = set()
+        
+        # Get the blockchain's nodes inside defined main_nodes
+        # And Add it to self.nodes
+        # [syncedNode.add(x) for x in main_nodes]
+        syncedNode.update(main_nodes)
+        syncedNode.add(f"{host}:{port}")
+        for node in main_nodes:
+            # if node != f"{host}:{str(port)}": 
+            def syncGetNode():        
+                try:
+                    print(f"http://{node}/get_nodes/")
+                    resNodes = requests.get(f"http://{node}/get_nodes/").json()
+                    resNodesSet = set()
+                    resNodesSet.update(resNodes)
+                    syncedNode.update(resNodes) 
+                    # if syncedNode != resNodesSet:
+                    #     try:
+                    #         def addNodes():
+                    #             res = requests.get(f"http://{node}/add_nodes/{host}:{port}")
+                                
+                    #         addNodesTread = threading.Thread(target=addNodes)
+                    #         addNodesTread.start()
+                    #     except:
+                    #         print("Get nodes fail")
+                except:
+                    print(f"Connection refused on {node}")
+                    syncedNode.remove(node)
+            syncGetNodeThread = threading.Thread(target=syncGetNode)
+            syncGetNodeThread.start()
+        
+        self.nodes = syncedNode
+        
+
+        #TODO
+        #Compare all nodes' blockchain length. if other chian's length is longer, replace with it.
+        newNodes = set()
+        copySelfNode = set()
+        copySelfNode.update([node for node in self.nodes])
+        for node in copySelfNode:
+            chain = None
+            if node != f"{host}:{str(port)}":
+                try:
+                    rawChain = requests.get(f"http://{node}/get_chain/").content
+                    chain = pickle.loads(rawChain)
+                    newNodes.add(node)
+                except:
+                    print(f"Cannot connect to {node}, removed from nodes list")
+
+            if chain and len(chain.blocks)>len(self.blocks):
+                print(self.validateChain(chain))
+                if self.validateChain(chain):
+                    print(f"Height of {node}'s chain({len(chain.blocks)}) >  height of {host}:{str(port)}'s chain ({len(self.blocks)}), self.chain will be replaced.")
+                    self.blocks = chain.blocks
+                    print("Done")
+            
+            elif chain and len(chain.blocks)<len(self.blocks):
+
+                # create a sync clousure
+                def askNodeToSync():
+                    print(f"Height of {node}'s chain({len(chain.blocks)}) <  height of {host}:{str(port)}'s chain ({len(self.blocks)})\nAsk node to sync")
+                    try:
+                        res = requests.get(f"http://{node}/sync_nodes/")
+                        if res.status_code == 200:
+                            print("Done")
+                        else:
+                            print("Fail")
+                    except:
+                        print("Fail")
+                syncRequest = threading.Thread(target=askNodeToSync)
+                syncRequest.start()
+                    
+            self.nodes = newNodes
+            self.nodes.add(f"{host}:{port}")
+        
+
+        
+
+
+
+
+    
+
+            
+            
+
+        
+
 
 
 
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import transaction as tx
 import blockchain
-from flask import Flask, jsonify
-import pickle
+from flask import Flask, jsonify, request
+import pickle, sys, json, requests
+import threading
 
 app = Flask(__name__)
+host = "0.0.0.0"
 
-chain = blockchain.Blockchain()
+if len(sys.argv) < 2:
+    print("please defind port by --port")
+else:
+    if sys.argv[1].startswith("--"):
+        option = sys.argv[1][2:]
+        if option == "port":
+            port = int(sys.argv[2])
+    else:
+        print("please defind port by --port")
+
+chain = blockchain.Blockchain(host, port)
 
 @app.route("/mine_block/<name>/<amount>", methods = ["GET"])
 def mindCoinBaseBlock(name, amount, chain=chain):
+    chain.syncNode(host, port)
     coin = tx.Transaction("coinbase", {name: int(amount)})
 
     byteCoin = pickle.dumps(coin)    
     newBlock = blockchain.Block(chain.getLastID_Hash()[0]+1 ,byteCoin, chain.getLastID_Hash()[1])
     newBlock.mineBlock()
     chain.addBlock(newBlock)
+    chain.syncNode(host, port)
     
     response = {
         "sender": name,
@@ -28,8 +42,13 @@ def mindCoinBaseBlock(name, amount, chain=chain):
     jsonify(response)
     return jsonify(response)
 
-@app.route("/transfer/<sender>/<to>/<amount>", methods = ["GET"])
-def mindTransactionBlock(sender, to, amount, chain=chain):
+@app.route("/transfer/", methods = ["POST"])
+def mindTransactionBlock():
+    chain.syncNode(host, port)
+    req = request.get_json()
+    sender = req["sender"]
+    to = req["to"]
+    amount = req["amount"]
     trx = tx.Transaction(sender, {to: int(amount)})
     balance = trx.updateInputs(chain)    
     
@@ -38,6 +57,7 @@ def mindTransactionBlock(sender, to, amount, chain=chain):
     
     newBlock.mineBlock()
     chain.addBlock(newBlock)
+    chain.syncNode(host, port)
     response = {
         "sender": sender,
         "amount": amount,
@@ -51,12 +71,43 @@ def mindTransactionBlock(sender, to, amount, chain=chain):
             "txid": trx.id
         }
     }
-    return jsonify(response)
+    return jsonify(req)
 
 @app.route("/printBlocks/", methods = ["GET"])
 def printBlocks(chain=chain):
     blocksData = chain.printBlocks()
     return jsonify(blocksData)
+
+
+#TODO 
+@app.route("/get_nodes/", methods = ["GET"])
+def get_nodes():
+    nodes = [node for node in chain.nodes]
+    return jsonify(nodes)
+
+@app.route("/get_chain/", methods = ["GET"])
+def get_chain():
+    return pickle.dumps(chain)
+
+@app.route("/sync_nodes/", methods = ["GET"])
+def sync_nodes():
+    chain.syncNode(host, port)
+    return jsonify([node for node in chain.nodes])
+
+# @app.route("/add_nodes/<node>", methods = ["GET"])
+# def add_nodes(node):
+#     # def outputNode():
+#     nodesToAdd = requests.get(f"http://{node}/output_nodes").json()
+#     chain.nodes.update(nodesToAdd)
+#     # outputNodeThread = threading.Thread(target=outputNode)
+#     # outputNodeThread.start()
+#     return jsonify(nodesToAdd)
+
+# @app.route("/output_nodes/", methods = ["GET"])
+# def output_nodes():
+#     # chain.nodes
+#     return jsonify([node for node in chain.nodes])
+
 
 
 if __name__ == "__main__":        
@@ -70,7 +121,8 @@ if __name__ == "__main__":
     """ 
 
     # Run Flask 
-    app.run(host="0.0.0.0", port=5000)
+
+    app.run(host=host, port=port)
     """
     Equivalant as doing following step on cli
     1. export FLASK_APP=main.py (Add FLASK_APP into env variable)
